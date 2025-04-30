6dbe9feac9ab18618c8f9fbc6f4359b893b2611d
Ruth <rut@leader.codes>
2025-05-01 02:26:35 +0300

    add missing import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { PhishingSimulationModule } from './phishing-simulation/phishing-simulat
 import { MailerModule } from '@nestjs-modules/mailer';
 import { PhishingTrackingModule } from './phishing-tracking/phishing-tracking.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-
+    MongooseModule.forRoot(
+      'mongodb+srv://rutc3857:rutc3857@try.obtcfs2.mongodb.net/?retryWrites=true&w=majority&appName=try',
+      { dbName: 'dbA' },
+    ),
     MailerModule.forRoot({
       transport: {
         host: process.env.EMAIL_HOST,
--- src/phishing-tracking/phishing-tracking.module.ts
+++ src/phishing-tracking/phishing-tracking.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PhishingTrackingService } from './phishing-tracking.service';
 import { PhishingTrackingController } from './phishing-tracking.controller';
+import { PhishingTracking, PhishingTrackingSchema } from './schemas/phishing-tracking.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  
+  imports: [
+    MongooseModule.forFeature([{ name: PhishingTracking.name, schema: PhishingTrackingSchema }]),
+  ],
   controllers: [PhishingTrackingController],
   providers: [PhishingTrackingService],
 })
