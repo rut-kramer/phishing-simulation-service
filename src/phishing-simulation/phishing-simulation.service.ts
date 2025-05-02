ac65cc3248ff1d8a7a2c105b88a082033eb3f586
Ruth <rut@leader.codes>
2025-05-02 16:21:04 +0300

    Fix trackingUrl in sendEmail

--- src/phishing-simulation/phishing-simulation.service.ts
+++ src/phishing-simulation/phishing-simulation.service.ts
@@ -6,31 +6,8 @@ import { MailerService } from '@nestjs-modules/mailer';
 export class PhishingSimulationService {
   constructor(private readonly mailService: MailerService) {}
 
-  //   async sendEmail(userEmail: string, userId: string): Promise<void> {
-  //     const trackingUrl = `http://yourdomain.com/api/phishing/track-click/${userId}`;
-
-  //     const transporter = nodemailer.createTransport({
-  //       service: 'gmail',
-  //       auth: {
-  //         user: this.configService.get('EMAIL_USER'),
-  //         pass: this.configService.get('EMAIL_PASS'),
-  //       },
-  //     });
-
-  //     const mailOptions = {
-  //       from: this.configService.get('EMAIL_USER'),
-  //       to: userEmail,
-  //       subject: 'Security Alert',
-  //       html: `<p>Click <a href="${trackingUrl}">here</a> to verify your account.</p>`,
-  //     };
-
-  //     await transporter.sendMail(mailOptions);
-  //     console.log('Phishing simulation email sent to', userEmail);
-  //   }
-
   sendEmail(userEmail: string) {
-    
-    const trackingUrl = `http://yourdomain.com/api/phishing/track-click?email=${encodeURIComponent(userEmail)}`;
+    const trackingUrl = `http://localhost:3001/phishing-tracking/track-click?email=${encodeURIComponent(userEmail)}`;
 
     this.mailService.sendMail({
       from: process.env.EMAIL_FROM,
